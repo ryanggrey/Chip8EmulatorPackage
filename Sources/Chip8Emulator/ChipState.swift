7ff71da279981cdab2341b16dfeb6503ded28df2
Ryan Grey <rgrey@scottlogic.co.uk>
2021-02-12 16:35:03 +0000

    Move emulator files from WatchOS project

---
+++ Sources/Chip8Emulator/ChipState.swift
@@ -0,0 +1,45 @@
+//
+//  ChipState.swift
+//  CHIP-8
+//
+//  Created by Ryan Grey on 01/02/2021.
+//
+
+import Foundation
+
+struct ChipState {
+    // 4K memory
+    public var ram = [Byte](repeating: 0, count: 4096)
+    // 16 variables
+    public var v = [Byte](repeating: 0, count: 16)
+    public var i: Word = 0
+    // Roms loaded into 0x200. Memory prior to this is reserved (for font etc)
+    public var pc: Word = 0x200
+    public var screen = Chip8Screen()
+    public var delayTimer: TimeInterval = 0
+    public var soundTimer: TimeInterval = 0
+    // 12 or 16 sized stack in real Chip-8, but allow this to grow dynamically
+    public var stack = [Word]()
+    // stack of currently pressed keys
+    // last pressed key at top of stack
+    // first pressed key on bottom of stak
+    public var downKeys = NSMutableOrderedSet()
+
+    public var isAwaitingKey = false
+    public var needsRedraw = false
+
+    public var shouldPlaySound: Bool {
+        return soundTimer > 0
+    }
+
+    public var currentOp: Word {
+        let byte1 = ram[pc]
+        let byte2 = ram[pc + 1]
+        let nibble1 = byte1 >> 4 // shift everything right by 4 bits, prefixing with 0s
+        let nibble2 = byte1 & 0x0F // & with 00001111, causing the 1st nibble to be 0ed and the 2nd nibble to be preserved
+        let nibble3 = byte2 >> 4
+        let nibble4 = byte2 & 0x0F
+        let opWord = Word(nibbles: [nibble1, nibble2, nibble3, nibble4])
+        return opWord
+    }
+}
---
+++ Sources/Chip8Emulator/RomLoader.swift
@@ -0,0 +1,39 @@
+//
+//  RomLoader.swift
+//  CHIP-8
+//
+//  Created by Ryan Grey on 23/01/2021.
+//
+
+import Foundation
+
+public struct RomLoader {
+    static func loadRam(from rom: [Byte]) -> [Byte] {
+        var ram = [Byte](repeating: 0, count: 4096)
+
+        let font = Font.bytes
+        ram.replaceSubrange(0..<font.count, with: font)
+
+        ram.replaceSubrange(0x200..<0x200+rom.count, with: rom)
+
+        return ram
+    }
+
+    public static func read(romPath path: String) -> [Byte] {
+        guard FileManager.default.fileExists(atPath: path) else {
+            print("Rom not found: " + path)
+            return []
+        }
+
+        do {
+            let url = URL(fileURLWithPath: path)
+            let romData = try Data(contentsOf: url)
+            let rom = [Byte](romData)
+            return loadRam(from: rom)
+        } catch {
+            // contents could not be loaded
+            print("Rom not found: " + path)
+            return []
+        }
+    }
+}
